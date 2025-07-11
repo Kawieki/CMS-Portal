1f9332dd4b23cb6c47147699aca8bae451fea5e1
Łukasz Wiszniewski <lwiszniewski.code@gmail.com>
2025-07-11 16:20:55 +0200

    add Update method

--- CMS/Domain/Entities/Article.cs
+++ CMS/Domain/Entities/Article.cs
@@ -1,5 +1,4 @@
 using Domain.Enums;
-using Domain.Helpers;
 
 namespace Domain.Entities;
 
@@ -9,44 +8,66 @@ public class Article
     public string Title { get; private set; } = null!;
     public string Content { get; private set; } = null!;
     public string Author { get; private set; } = null!; 
-    public string Slug { get; private set; } = null!;
+    public string Slug { get; private set; }
     public ArticleStatus Status { get; private set; }
     public DateTime CreatedAt { get; private set; }
+    public Guid CategoryId { get; set; }
+    private const int MinContentLength = 10;
     
-    public Article(string title, string content, string author)
+    public Article(string title, string content, string author, string slug)
     {
         Id = Guid.NewGuid();
         SetTitle(title);
         SetContent(content);
         SetAuthor(author);
+        Slug = slug;
         CreatedAt = DateTime.UtcNow;
         Status = ArticleStatus.Draft;
     }
     
+    public void Update(string? title, string? content, string? author, Guid? categoryId)
+    {
+        if (!string.IsNullOrWhiteSpace(title))
+            SetTitle(title);
+
+        if (!string.IsNullOrWhiteSpace(content))
+            SetContent(content);
+
+        if (!string.IsNullOrWhiteSpace(author))
+            SetAuthor(author);
+
+        if (categoryId.HasValue)
+            SetCategory(categoryId.Value);
+    }
+
     public void Publish()
     {
         Status = ArticleStatus.Published;
     }
-    
-    public void SetAuthor(string author)
-    {
-        if (string.IsNullOrWhiteSpace(author))
-            throw new ArgumentException("Author cannot be empty.", nameof(author));
-        Author = author;
-    }
-    
-    public void SetTitle(string title)
+
+    private void SetTitle(string title)
     {
         if (string.IsNullOrWhiteSpace(title))
             throw new ArgumentException("Title cannot be empty.", nameof(title));
         Title = title;
-        Slug = SlugGenerator.Generate(title);
     }
 
-    public void SetContent(string content)
+    private void SetContent(string content)
     {
-        if (content == null || content.Length < 10)
-            throw new ArgumentException("Content must have at least 10 characters.");
+        if (string.IsNullOrWhiteSpace(content))
+            throw new ArgumentException("Content cannot be empty.");
+        if(content.Length < MinContentLength)
+            throw new ArgumentException($"Content must be at least {MinContentLength} characters long.", nameof(content));
         Content = content;
+    }
+
+    private void SetAuthor(string author)
+    {
+        Author = author;
+    }
+
+    private void SetCategory(Guid categoryId)
+    {
+        CategoryId = categoryId;
     }
 }
