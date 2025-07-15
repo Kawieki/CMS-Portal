63e129014b820531221273c3bf80aab9357dcc4b
s31083 <s31083@pjwstk.edu.pl>
2025-07-15 03:09:39 +0200

    method logic moved to helper class

--- CMS/Application/Validation/UpdateArticleValidator.cs
+++ CMS/Application/Validation/UpdateArticleValidator.cs
@@ -4,14 +4,9 @@ namespace Application.Validation;
 
 public class UpdateArticleValidator : IValidator<UpdateArticleDto>
 {
-    private const int MinContentLength = 10;
-    
-    public async Task<List<string>> ValidateAsync(UpdateArticleDto dto)
-    { 
-        var errors = new List<string>();
-        if (dto.Content is { Length: < MinContentLength })
-            errors.Add($"Content must be at least {MinContentLength} characters long");
-            
-        return await Task.FromResult(errors);
+    public Task<List<string>> ValidateAsync(UpdateArticleDto dto)
+    {
+        var errors = ArticleValidationRules.Validate(dto.Title, dto.Content, dto.Author);
+        return Task.FromResult(errors);
     }
 }
