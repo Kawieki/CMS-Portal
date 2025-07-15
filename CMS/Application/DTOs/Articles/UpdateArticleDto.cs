61a2b534bb36b423e646e91801b4e51438be5994
s31083 <s31083@pjwstk.edu.pl>
2025-07-15 03:09:21 +0200

    removed nullable types

--- CMS/Application/DTOs/Articles/UpdateArticleDto.cs
+++ CMS/Application/DTOs/Articles/UpdateArticleDto.cs
@@ -2,8 +2,9 @@ namespace Application.DTOs.Articles;
 
 public class UpdateArticleDto
 {
-    public string? Title { get; set; }
-    public string? Content { get; set; }
-    public string? Author { get; set; }
+   
+    public string Title { get; set; } = null!;
+    public string Content { get; set; }  = null!;
+    public string Author { get; set; } = null!;
     public Guid? CategoryId { get; set; }
 }
